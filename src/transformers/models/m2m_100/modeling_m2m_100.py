7a357f2cdaf812e5fe3119e4e5fe131c249e1a90
Yao Lu <95luyao@gmail.com>
2024-10-22 15:21:13 +0800

    Update modeling_m2m_100.py

--- src/transformers/models/m2m_100/modeling_m2m_100.py
+++ src/transformers/models/m2m_100/modeling_m2m_100.py
@@ -217,7 +217,10 @@ class M2M100Attention(nn.Module):
         self.out_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
 
     def _shape(self, tensor: torch.Tensor, seq_len: int, bsz: int):
-        return tensor.view(bsz, seq_len, self.num_heads, self.head_dim).transpose(1, 2).contiguous()
+        tensor_clone = tensor.clone()
+        output = tensor_clone.view(bsz, seq_len, self.num_heads, self.head_dim).transpose(1, 2).contiguous()
+        return output
+        # return tensor.view(bsz, seq_len, self.num_heads, self.head_dim).transpose(1, 2).contiguous()
 
     def forward(
         self,
