0acf044b830ac23e5336282bd02fb83e1378c574
Yao Lu <95luyao@gmail.com>
2024-10-22 15:31:16 +0800

    Update modeling_m2m_100.py

--- src/transformers/models/m2m_100/modeling_m2m_100.py
+++ src/transformers/models/m2m_100/modeling_m2m_100.py
@@ -221,7 +221,6 @@ class M2M100Attention(nn.Module):
     def _shape(self, tensor: torch.Tensor, seq_len: int, bsz: int):
         torch.compiler.cudagraph_mark_step_begin()
         output = tensor.view(bsz, seq_len, self.num_heads, self.head_dim).transpose(1, 2).contiguous()
-        torch.compiler.cudagraph_mark_step_end()
         return output
 
     def forward(
